cf29f6f321301c12aadeb714d8f6d6c032e89b00
Ciera Lopez <123427746+cieralopez818@users.noreply.github.com>
2023-03-04 10:05:27 -0700

    I pushed some cool files!

--- index.js
+++ index.js
@@ -2,26 +2,17 @@ class dog {
     constructor (name, breed){
         this.name= name;
         this.breed= breed;
-
     }
-    describe (){
-        return' $(this.name) size$(this.breed)';
+
+    describe() {
+        //console.log(`${this.name} size ${this.breed}`)
+        return `${this.name} size ${this.position}`;
     }
 }
 class breed {
     constructor (name){
         this.breed= breed;
         this.size= [];
-    }
-    add breed (breed) {
-        if (breed instance of breed) {
-            this.breeds.push(breed);
-    } else {
-        throw new Error ("You can only add an instance of breeds. Argument is not a breed; $ {breed}');
-    }
-}
-    describe () {
-        return'${this.name} breed.;
     }
 }
     class menu {
@@ -31,73 +22,80 @@ class breed {
         }
         start (){
             let selection= this.showMainMenuOptions ();
-                while (selection is 0) {
+                while (selection != 0) {
                     switch (selection) {
-                    case '1';
+                    case '1':
                         this.createBreed ();
                         break;
-                    case '2';
+                    case '2':
                         this.viewBreed();
                         break;
-                    case '3';
+                    case '3':
                         this.deleteBreed();
                         break;
-                    default; 
+                    case '4':
+                        this.displayBreed();
+                    default: 
                         selection= 0;            
         }
-        selection= this.showMainMenuOptions (),
+        selection= this.showMainMenuOptions ()
     }
         alert('Goodbye!');
         }     
 showMainMenuOptions (){
-    return prompt {
+    return prompt (`
     0)exit
     1)createBreed
     2)viewBreed 
     3)deleteBreed
     4)display allBreeds
-);
+`);
     }
-}
-showMainMenuOptions (breed info) {
-    return promt{
+
+showMainMenuOptions (breedInfo) {
+    return prompt (`
         0)back 
         1)createBreed
-        2)viewBreed
-............... 
+        2)viewBreed 
+...............
         ${breedInfo}
-    ');'
+`);
     }
-}
     displayBreeds (){
-        let breedString = "    ";
-        for let i = 0; i < this.breeds.length; i++) {
-            breedString + =i +')' + this.breeds [i].names+ '\n';
+        let breedString = '    ';
+        for (let i = 0; i < this.breeds.length; i++) {
+            breedString += i+ ') ' + this.breeds[i].names+ '\n';
         }
         alert (breedString);
     }
     createBreed (){
-        let name= prompt ('Enter name fore new breed';);
-        this.breeds.push (new Breed (name));
+        let name= prompt ('Enter name fore new breed');
+        this.breeds.push (new Breed(name));
     }
     viewBreed (){
-        let index = prompt ('Enter the index of the breed you wish to view';);
+        let index = prompt ('Enter the index of the breed you wish to view');
         if (index > -1 && index < this.breeds.length){
             this.selectedBreed = this.breed (index);
-            let description = 'breedName;' + this.selectedBreed.name + '\n';
+            let description = 'Breed Name: ' + this.selectedBreed.name + '\n';
             for (let i= 0; i < this.selectedBreed.dogs.length; i++){
-                description + = i +') + this.selectedBreed.dogs[i].name + '_'
+                description += i +')' + this.selectedBreed.dogs[i].name + ''
                 + this.selectedBreed.dogs[i]. size + '\n';
-        }
+            }
         let selection= this.showBreedMenuOptions (description);
             switch (selection) {
-                case '1';
+                case '1':
                     this.createBreed ();
                     break;
-                case '2';
+                case '2':
                     this.viewBreed ();
-            }
+           }   } }
+    deleteBreed (){
+        let index = prompt ('Enter the index of the breed you wish to delete:');
+        if (index > -1 && index < this.breeds.length) {
+            this.breeds.splice(index,1);
+        }
     }
+
     createBreed (){
         let dog= prompt ('Enter name for new dog:');
         let size= prompt ('Enter size for new dog:');
@@ -109,5 +107,6 @@ showMainMenuOptions (breed info) {
             this.selectedBreed.dogs.splice (index, 1);
         }
     }
-    let menu= new Menu/ );
-    menu.start ();
+}   
+    let menu= new Menu ();
+    menu.start();
